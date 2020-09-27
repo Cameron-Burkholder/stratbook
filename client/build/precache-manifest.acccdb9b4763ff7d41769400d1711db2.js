1b1078ab4dd5d33e47d271d2b896cde87307c71b
Cameron-Burkholder <cburkholder19@gmail.com>
2020-09-27 15:39:10 -0400

    09-27-2020 15:39 - Add built to github for deploy.

---
+++ client/build/precache-manifest.acccdb9b4763ff7d41769400d1711db2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50b8161a36877431e2f4bb18cf9eb8ee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "643f3774213a3a3f9923",
+    "url": "/static/js/2.60760fd3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.60760fd3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6cd539a2659dca4b6b24",
+    "url": "/static/js/main.b3d7f3f4.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
